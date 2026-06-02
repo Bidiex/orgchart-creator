c273cbb7ad5ab4f623facad97acb454efbc42dc7
Bidiex <bidiex@hotmail.com>
2026-06-01 21:43:58 -0500

    refactor: update base URL to orgchart-creator and implement custom 404 redirect handling

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/orgchart-studio/',
+  base: '/orgchart-creator/',
   plugins: [
     react(),
     tailwindcss()
